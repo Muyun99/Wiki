55a08448dd3bbe473c9fa52c93bb0dafa2b52b79
muyun99 <27944418+Muyun99@users.noreply.github.com>
2021-03-08 19:18:26 +0800

    update description

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,9 +6,8 @@ module.exports = {
   theme: 'vdoing', // 使用依赖包主题
   // theme: require.resolve('../../theme-vdoing'), // 使用本地主题
 
-  title: "Evan's blog",
+  title: "Muyun's wiki",
   description: 'web前端技术博客,简洁至上,专注web前端学习与总结。JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github等技术文章。',
-  // base: '/', // 格式：'/<仓库名>/'， 默认'/'
   markdown: {
     lineNumbers: true, // 代码行号
   },
